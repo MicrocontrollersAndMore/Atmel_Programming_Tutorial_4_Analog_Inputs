050f103ebdf440c90d52374968a9f62edf086319
MicrocontrollersAndMore <cdahms123@gmail.com>
2015-11-13 21:02:51 -0500

    1

---
+++ ADC_WithInterruptFreeRunning.c
@@ -0,0 +1,90 @@
+// ADC_WithInterrupt.c
+
+// 10k ohm pot on PC5/ADC5 (pin 28)
+// 8 LEDs on Port D pins
+
+#ifndef F_CPU					// if F_CPU was not defined in Project -> Properties
+#define F_CPU 1000000UL			// define it now as 1 GHz unsigned long
+#endif
+
+#include <avr/io.h>				// this is always included in AVR programs
+#include <avr/interrupt.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+int main(void) {
+	
+	DDRD = 0xFF;				// set Port D pins for output
+	
+    /*
+	ADMUX - ADC Multiplexer Selection Register
+	
+	bit          7           6          5         4        3         2          1          0
+	name       REFS1       REFS0      ADLAR       -       MUX3      MUX2       MUX1       MUX0
+	set to       0           1          1         0        0         1          0          1
+	
+	REFS1 = 0    use AVCC for reference voltage
+	REFS0 = 1
+	
+	ADLAR = 1    left justify ADC result in ADCH/ADCL
+	
+	bit 4 = 0
+	
+	MUX3 = 0     use PC5/ADC5 (pin 28) for input
+	MUX2 = 1
+	MUX1 = 0
+	MUX0 = 1
+	*/
+	ADMUX = 0b01100101;
+	
+	/*
+	ADCSRA - ADC Control and Status Register A
+	
+	bit          7           6            5          4          3            2           1           0
+	name        ADEN        ADSC        ADATE       ADIF       ADIE        ADPS2       ADPS1       ADPS0
+	set to       1           0            1          0          1            0           1           1
+	
+	ADEN = 1     enable ADC
+	ADSC = 0     don't start ADC yet
+	ADATE = 1    enable ADC auto trigger (i.e. use single conversion mode)
+	ADIF = 0     don't set ADC interrupt flag
+	ADIE = 1     enable ADC interrupt
+	
+	ADPS2 = 0
+	ADPS1 = 1    1 MHz clock / 8 = 125 kHz ADC clock
+	ADPS0 = 1
+	*/
+	ADCSRA = 0b10101011;
+	
+	/*
+	ADCSRB - ADC Control and Status Register B
+	
+	bit         7           6           5           4           3         2           1           0
+	name        -          ACME         -           -           -       ADTS2       ADTS1       ADTS0
+	set to      0           0           0           0           0         0           0           0
+	
+	bit 7 = 0
+	ACME = 0     don't enable analog comparator multiplexer
+	bit 5 = 0
+	bit 4 = 0
+	bit 3 = 0
+	ADTS2 = 0
+	ADTS1 = 0    free running mode
+	ADTS0 = 0
+	*/
+	ADCSRB = 0b00000000;
+	
+	sei();			// enable interrupts
+	
+	ADCSRA |= (1 << ADSC);		// start ADC
+	
+	while (1) { }				// infinite loop
+	
+	return(0);					// should never get here, this is to prevent a compiler warning
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+ISR(ADC_vect) {
+	PORTD = ADCH;				// assign contents of ADC high register to Port D pins
+}
+
+
